ead855c89c5a00179a5e58c98f58d51a635e7844
Vladimir Frolov <vova@frolov.pp.ru>
2021-07-23 17:06:57 +0300

    edit

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # LiteMath
-Lightweight and simple math library for graphics
+Lightweight single source math library for graphics without issues
 
-## How to get code coverage
+## How to get code coverage in HTML
 
 * sudo apt-get install gcovr
 * mkdir build && cd build 
-* cmake -DCMAKE_BUILD_TYPE=Debug ..
+* cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=ON ..
+* make -j 8
 * make coverage
 * see results in 'coverage' directory
 
--- cmake_modules/CodeCoverage.cmake
+++ cmake_modules/CodeCoverage.cmake
@@ -521,10 +521,11 @@ function(setup_target_for_coverage_gcovr_html)
         -o ${Coverage_NAME}/index.html
     )
 
-    #set(GCOVR_CMD_FROL
-    #    ${GCOVR_PATH} -r ${BASEDIR} ${GCOVR_ADDITIONAL_ARGS}
-    #    ${GCOVR_EXCLUDE_ARGS} --object-directory=${PROJECT_BINARY_DIR} 
-    #)
+    set(GCOVR_CMD_FROL
+        ${GCOVR_PATH} --xml -r ${BASEDIR} ${GCOVR_ADDITIONAL_ARGS}
+        ${GCOVR_EXCLUDE_ARGS} --object-directory=${PROJECT_BINARY_DIR} 
+        -o ${Coverage_NAME}/coverage.xml
+    )
 
     if(CODE_COVERAGE_VERBOSE)
         message(STATUS "Executed command report")
@@ -546,7 +547,7 @@ function(setup_target_for_coverage_gcovr_html)
         COMMAND ${GCOVR_HTML_EXEC_TESTS_CMD}
         COMMAND ${GCOVR_HTML_FOLDER_CMD}
         COMMAND ${GCOVR_HTML_CMD}
-        #COMMAND ${GCOVR_CMD_FROL}
+        COMMAND ${GCOVR_CMD_FROL}
 
         BYPRODUCTS ${PROJECT_BINARY_DIR}/${Coverage_NAME}/index.html  # report directory
         WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
