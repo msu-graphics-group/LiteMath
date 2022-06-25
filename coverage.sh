36a66b650af143b8d4ba29b9d6722e4f9484c7f9
Vladimir Frolov <vova@frolov.pp.ru>
2022-06-25 14:04:41 +0300

    enabvle stb_image for code coverage

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,5 @@
 sudo apt-get install gcovr
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=ON ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=ON DUSE_STB_IMAGE=ON .. 
 make -j 8
 make coverage
