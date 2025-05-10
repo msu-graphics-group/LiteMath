a3764cbcf836ff5278f963b9c77179da339c98e7
vova@frolov.pp.ru <vova@frolov.pp.ru>
2025-05-10 16:38:34 +0300

    missed perspectivematrix

--- LiteMath.h
+++ LiteMath.h
@@ -3258,6 +3258,37 @@ namespace LiteMath
                          1.0f });
     return M;
   }
+
+  static inline float4x4 perspectiveMatrix(float fovy, float aspect, float zNear, float zFar)
+  {
+    const float ymax = zNear * tanf(fovy * 3.14159265358979323846f / 360.0f);
+    const float xmax = ymax * aspect;
+    const float left = -xmax;
+    const float right = +xmax;
+    const float bottom = -ymax;
+    const float top = +ymax;
+    const float temp = 2.0f * zNear;
+    const float temp2 = right - left;
+    const float temp3 = top - bottom;
+    const float temp4 = zFar - zNear;
+    float4x4 res;
+    res.m_col[0] = float4{ temp / temp2, 0.0f, 0.0f, 0.0f };
+    res.m_col[1] = float4{ 0.0f, temp / temp3, 0.0f, 0.0f };
+    res.m_col[2] = float4{ (right + left) / temp2,  (top + bottom) / temp3, (-zFar - zNear) / temp4, -1.0 };
+    res.m_col[3] = float4{ 0.0f, 0.0f, (-temp * zFar) / temp4, 0.0f };
+    return res;
+  }
+
+  // http://matthewwellings.com/blog/the-new-vulkan-coordinate-system/
+  //
+  static inline float4x4 OpenglToVulkanProjectionMatrixFix()
+  {
+    float4x4 res;
+    res(1,1) = -1.0f;
+    res(2,2) = 0.5f;
+    res(2,3) = 0.5f;
+    return res;
+  }
   
   static inline float4 packFloatW(const float4& a, float data) { return blend(a, float4(data),            uint4{0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF,0}); }
   static inline float4 packIntW(const float4& a, int data)     { return blend(a, as_float32(int4(data)),  uint4{0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF,0}); }
