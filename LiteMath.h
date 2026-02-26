70695cebbe895a8aa7ee30f741b9d93fc31bb708
VladimirFrolov <vova@frolov.pp.ru>
2026-02-26 22:25:34 +0300

    static inline

--- LiteMath.h
+++ LiteMath.h
@@ -3747,9 +3747,9 @@ namespace LiteMath
 #ifdef _OPENMP
 #include <omp.h>
 #else
-static int omp_get_num_threads() { return 1; }
-static int omp_get_max_threads() { return 1; }
-static int omp_get_thread_num()  { return 0; }
+static inline int omp_get_num_threads() { return 1; }
+static inline int omp_get_max_threads() { return 1; }
+static inline int omp_get_thread_num()  { return 0; }
 #endif
 
 namespace LiteMath
